1d55e679d9fe4945af1fe293d67e4c1ec39eb0be
pr00thmatic <vengadoravg@gmail.com>
2021-05-22 23:22:12 -0400

    (wip) pan and egg

--- Assets/Scripts/Pan.cs
+++ Assets/Scripts/Pan.cs
@@ -6,7 +6,15 @@ public class Pan : MonoBehaviour, IOileable {
   [Header("Information")]
   public float oil;
 
+  [Header("Initialization")]
+  public Estufable estufable;
+  public ParticleSystem steam;
+
   public void AddOil (float amount) {
     oil += amount;
+  }
+
+  void Update () {
+
   }
 }
