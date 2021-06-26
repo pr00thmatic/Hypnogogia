bac798d6ee5fddb375ad4dbfef5e8f7509107618
pr00thmatic <vengadoravg@gmail.com>
2021-06-26 12:13:23 -0400

    pitch deck v2

--- Assets/Scripts/CloudConfiner.cs
+++ Assets/Scripts/CloudConfiner.cs
@@ -5,10 +5,10 @@ using System.Collections.Generic;
 public class CloudConfiner : MonoBehaviour {
   void Update () {
     Vector3 relativePosition = Camera.main.transform.InverseTransformPoint(transform.position);
-    if (relativePosition.x > 20) {
-      relativePosition.x = -19;
-    } else if (relativePosition.x < -20) {
-      relativePosition.x = 19;
+    if (relativePosition.x > 36) {
+      relativePosition.x = -35;
+    } else if (relativePosition.x < -36) {
+      relativePosition.x = 35;
     }
     transform.position = Camera.main.transform.TransformPoint(relativePosition);
   }
