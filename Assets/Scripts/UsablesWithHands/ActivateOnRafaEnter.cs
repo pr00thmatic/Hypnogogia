2e379de50ff2d89db4bdde769804593d6b401042
pr00thmatic <vengadoravg@gmail.com>
2022-02-26 09:08:09 -0400

    wip: convert colliders to 3D

--- Assets/Scripts/UsablesWithHands/ActivateOnRafaEnter.cs
+++ Assets/Scripts/UsablesWithHands/ActivateOnRafaEnter.cs
@@ -6,7 +6,7 @@ public class ActivateOnRafaEnter : MonoBehaviour {
   [Header("Initialization")]
   public GameObject toActivate;
 
-  void OnTriggerEnter2D (Collider2D c) {
+  void OnTriggerEnter (Collider c) {
     if (!c.GetComponent<Rafa>()) return;
     toActivate.SetActive(true);
   }
